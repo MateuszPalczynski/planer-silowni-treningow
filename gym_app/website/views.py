3d4c3dd18bf0748de1a029372a270c5660640405
yuliya_shuleika <yulia.shuleiko@gmail.com>
2025-03-22 00:25:08 +0100

    Added login logic to the existing login page

--- gym_app/website/views.py
+++ gym_app/website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Widok logowania
 class LoginPageView(LoginView):
@@ -19,4 +21,19 @@ def register(request):
 
 def home(request):
     return render(request, 'home.html')
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password")
+    
+    return render(request, 'login.html')
 
