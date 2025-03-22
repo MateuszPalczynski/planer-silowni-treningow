8a3857506a2add4441c070f387988a42d104e97b
Pau125 <106263567+Pau125@users.noreply.github.com>
2025-03-22 23:42:08 +0100

    Update views.py

--- gym_app/website/views.py
+++ gym_app/website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 # Widok rejestracji
@@ -12,7 +12,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
-            auth_login(request, user)
+            login(request, user)
             messages.success(request, f'Konto zostało utworzone dla {username}! Jesteś teraz zalogowany.')
             return redirect('home')
         else:
@@ -31,7 +31,7 @@ def login(request):
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            auth_login(request, user)
+            login(request, user)
             return redirect('home')
     else:
         form = AuthenticationForm()
