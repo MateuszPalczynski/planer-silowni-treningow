176306bd5277285df2d75c531ea9165f41760e17
yuliya_shuleika <yulia.shuleiko@gmail.com>
2025-03-22 11:11:19 +0100

    Used pre-built django login form

--- gym_app/website/views.py
+++ gym_app/website/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-
-# Widok logowania
-class LoginPageView(LoginView):
-    template_name = 'login.html'
 
 # Widok rejestracji
 def register(request):
@@ -25,15 +20,14 @@ def home(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             return redirect('home')
-        else:
-            messages.error(request, "Invalid username or password")
+    else:
+        form = AuthenticationForm()
     
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'form': form})
+
 
