e7e95a1fda70eb3ee35f83fbf50836e6d20feece
Pau125 <106263567+Pau125@users.noreply.github.com>
2025-03-22 15:40:22 +0100

    Update views.py

--- gym_app/website/views.py
+++ gym_app/website/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+
+# Widok logowania
+class LoginPageView(LoginView):
+    template_name = 'login.html'
 
 # Widok rejestracji
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')  # Przekierowanie do strony logowania po rejestracji
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Konto zostało utworzone dla {username}!')
+            return redirect('login')
+        else:
+            messages.error(request, 'Błąd podczas rejestracji. Spróbuj ponownie.')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
@@ -20,14 +29,14 @@ def home(request):
 
 def login(request):
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
             login(request, user)
             return redirect('home')
-    else:
-        form = AuthenticationForm()
+        else:
+            messages.error(request, "Invalid username or password")
     
-    return render(request, 'login.html', {'form': form})
-
-
+    return render(request, 'login.html')
