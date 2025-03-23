0fafb11cffccc746c7d9ff3e6109087c678f55dd
Mateusz Palczynski <236796@edu.p.lodz.pl>
2025-03-23 19:17:53 +0100

    adding logout functionality - this include backend in irls.py and some of new html code in home.html

--- gym_app/website/urls.py
+++ gym_app/website/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import user_login, register, home
+# import for logout
+from django.contrib.auth.views import LogoutView
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', user_login, name='login'),
     path('register/', register, name='register'),
+    # path for logout
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
